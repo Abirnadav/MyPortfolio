3658087652827019a5cc89ec2f3bdcae7ccd5df4
AbirNadav <pakakaw41@gmail.com>
2020-06-15 14:35:58 +0300

    Updates

---
+++ precache-manifest.e74fda652ba95b150ca437d125dd42c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5afc98613da0c538b16d",
+    "url": "/MyPortfolio/static/css/main.b0d423e8.chunk.css"
+  },
+  {
+    "revision": "5afc98613da0c538b16d",
+    "url": "/MyPortfolio/static/js/main.5afc9861.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/MyPortfolio/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "6311c3affd0fe28667be",
+    "url": "/MyPortfolio/static/js/runtime~main.6311c3af.js"
+  },
+  {
+    "revision": "a08c46b6e4a67ca57e01e40f12a703ef",
+    "url": "/MyPortfolio/static/media/bgi.a08c46b6.jpg"
+  },
+  {
+    "revision": "fe3bd97453cc49985ce816680419bb29",
+    "url": "/MyPortfolio/index.html"
+  }
+];
